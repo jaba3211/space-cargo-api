659dda0fb0448a112aee01842bfd458430366404
jaba3211 <jaba.sheklashvili11@gmail.com>
2023-12-08 19:29:46 +0400

    create parcels table

---
+++ database/migrations/2023_12_08_152343_create_parcels_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('parcels', function (Blueprint $table) {
+            $table->id();
+            $table->string('code', 60)->unique();
+            $table->string('price', 40);
+            $table->unsignedSmallInteger('quantity');
+            $table->string('store_address');
+            $table->text('comment');
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete()->cascadeOnUpdate();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('parcels');
+    }
+};
